b84cdc4518aa0e2c0879b956507c25f669b30adb
osboxes.org <Jackson>
2018-04-15 21:40:33 -0400

    paritally working

--- BoundedBuffer.c
+++ BoundedBuffer.c
@@ -8,12 +8,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <unistd.h>
 
 pthread_mutex_t m;
-pthread_cond_t full, empty;
+pthread_mutex_t f, g;
+pthread_cond_t fill, empty, inc;
 
 int num_consumer, num_producer, buffer_size, num_loops;
 int numfull = 0, fillptr = 0, useptr = 0;
+int filled = 0, got = 0;
 
 int *buffer;
 
@@ -31,15 +34,49 @@ int do_get() {
 	return tmp;
 }
 
+void *consumer (void *arg){
 
-void *producer (void *argv){
+	while(got < num_loops){
+		pthread_mutex_lock(&m);
+		while (numfull == 0 && got < num_loops){
+	//		printf("consumer %d got = %d\t",(int)pthread_self(),got);
+			printf("\tconsumer %d is waiting\n",(int)pthread_self());
+			pthread_cond_wait(&fill, &m);
+	//		printf("consumer %d woke up got = %d\n",(int)pthread_self(), got);
+		}
+		int tmp = do_get();
+		got++;
 
-	int i = 0;
-	for (i; i < num_loops; i++){
+		pthread_cond_signal(&empty);
+		pthread_mutex_unlock(&m);
+		printf("%d %d %d\n", tmp, (int)pthread_self(),got);
+
+	}
+
+}
+
+void *producer (void *arg){
+	int i;
+	for(i = 0; i < num_loops; i++){
+	//	printf("producer %d lock m\n", (int)pthread_self());
 		pthread_mutex_lock(&m);
 		while (numfull == buffer_size){
+	//		printf("producer %d is waiting\n", (int)pthread_self());
+			pthread_cond_wait(&empty, &m);
 
+	//		printf("producer %d woke up\n", (int)pthread_self());
 		}
+
+
+		do_fill(i);
+	//	pthread_mutex_lock(&f);
+//		filled++;
+//		pthread_mutex_unlock(&f);
+
+	//	printf("producer %d singal fill\n", (int)pthread_self());
+		pthread_cond_signal(&fill);
+	//	printf("producer %d unlock m\n", (int)pthread_self());
+		pthread_mutex_unlock(&m);
 	}
 
 }
@@ -60,8 +97,28 @@ int main (int argc, char* argv[]){
 
 	buffer = (int *) malloc(buffer_size * sizeof(int));
 	
-	printf("%d, %d, %d, %d\n", num_consumer, num_producer, buffer_size, num_loops);
+	printf("consumers: %d\n producers: %d\n buffer size: %d\n loops: %d\n", num_consumer, num_producer, buffer_size, num_loops);
 
+	pthread_t pid[10], cid[10];
+	int i;
+
+	for (i = 0; i < num_consumer; i++){
+		pthread_create(&cid[i], NULL, consumer, NULL);
+		printf("consumer %d = %d\n", i, (int) cid[i]);
+	}
+	for (i = 0; i < num_producer; i ++){
+		pthread_create(&pid[i], NULL, producer, NULL);
+		printf("producer %d = %d\n",i, (int)pid[i]);
+	}
+
+	for (i = 0; i < num_consumer; i++)
+		pthread_join(cid[i], NULL);
+	printf("consumers joined\n");
+
+	for (i = 0; i < num_producer; i++)
+		pthread_join(pid[i], NULL);
+	printf("producers joined\n");
+	
+	printf("parent complete\n");
 	return 0;
 }
-
